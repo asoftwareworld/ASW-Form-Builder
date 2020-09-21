c03730202707741dcad1c8d98b42086a92948665
asoftwareworld <asoftwareworld@gmail.com>
2020-09-21 15:59:00 +0530

    rename and refactor

--- src/app/layout/edit-property/button-property/button-property.component.ts
+++ src/app/layout/edit-property/button-property/button-property.component.ts
@@ -23,7 +23,7 @@ export class ButtonPropertyComponent implements OnInit {
     validateFormBuilder(): void {
         this.aswEditPropertyForm = this.formBuilder.group({
             tooltip: ['', [Validators.required]],
-            placeholder: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(20)]],
+            label: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(20)]],
             name: ['', [Validators.required]],
             type: ['', [Validators.required]],
             color: [],
@@ -35,7 +35,7 @@ export class ButtonPropertyComponent implements OnInit {
     editProperty(control: any): void {
         this.aswEditPropertyForm.setValue({
             tooltip: control.tooltip,
-            placeholder: control.placeholder,
+            label: control.label,
             name: control.name,
             type: control.type,
             color: control.color,
