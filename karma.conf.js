35a353ecb037b1a7a158ae9e93f4f4ea75b4404f
anishsharma8 <anishsharma927@gmail.com>
2020-09-23 17:17:58 +0530

    rename project

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Angular-Form-Bulder'),
+      dir: require('path').join(__dirname, './coverage/ASW-Form-Bulder'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
