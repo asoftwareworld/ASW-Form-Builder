9ebec57767392f9e3bff300e8c0baba8cf671b08
anishsharma8 <anishsharma927@gmail.com>
2020-09-23 18:26:13 +0530

    removed unused code

--- src/app/layout/form-builder/form-builder.component.spec.ts
+++ src/app/layout/form-builder/form-builder.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FormBuilderComponent } from './form-builder.component';
 
 describe('FormBuilderComponent', () => {
--- src/app/layout/form-builder/form-builder.component.ts
+++ src/app/layout/form-builder/form-builder.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, copyArrayItem } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import { Constants } from '../common/constants';
-import { ConfirmDialogComponent } from '../shared-components/confirm-dialog/confirm-dialog.component';
 import { ASWSettingsService } from '../shared-service/asw-settings.service';
-import { EditTextboxComponent } from '../edit-controls/edit-text-box/edit-text-box.component';
-import { EditMultiSelectComponent } from '../edit-controls/edit-multi-select/edit-multi-select.component';
-import { EditButtonComponent } from '../edit-controls/edit-button/edit-button.component';
-import { ButtonComponent } from './controls/button/button.component';
-import { debugOutputAstAsTypeScript } from '@angular/compiler';
 
 @Component({
   selector: 'asw-form-builder',
@@ -16,7 +10,7 @@ import { debugOutputAstAsTypeScript } from '@angular/compiler';
   styleUrls: ['./form-builder.component.scss']
 })
 export class FormBuilderComponent implements OnInit {
-	@ViewChild(ButtonComponent) button;
+	
 	constants: any = Constants;
 	availableControls: any[] = [];
 	formContainer: any[] = [];
@@ -38,22 +32,7 @@ export class FormBuilderComponent implements OnInit {
 							event.previousIndex,
 							event.currentIndex);
 		}
-  	}
-
-  	openDialog(item: any, i: any): void {
-		let dialogRef = this.dialog.open(ConfirmDialogComponent, {
-			width: '350px',
-			data: { name: item.name, message: this.constants.messages.waringMessage }
-		});
-		dialogRef.afterClosed().subscribe(result => {
-			if(result != undefined) {
-				var item = this.formContainer.find(x=>x.name == result);
-				console.log('The dialog was closed');
-				this.formContainer.splice(i, 1);
-				//this.animal = result;
-			}
-		});
-	}
+  	}  	
 
 	updatedControl(data: any) {
 		this.formContainer.splice(data.index, 1, data.control);
@@ -61,68 +40,5 @@ export class FormBuilderComponent implements OnInit {
 
 	deleteControl(index: any) {
 		this.formContainer.splice(index, 1);
-	}
-
-	editPropertyDialog(control: any, i: any): void {
-		switch(control.controlType) {
-			case 'multi-select':
-				this.editMultiSelectProperty(control, i);
-				break;
-			case 'button':
-				this.editButtonProperty(control, i);
-				break;
-			default:
-				this.editTextBoxProperty(control, i);
-				break;
-		}
-	}
-
-	editTextBoxProperty(control: any, i: any):void {
-		let dialogRef = this.dialog.open(EditTextboxComponent, {
-			disableClose: true,
-			width: '744px',
-			data: control
-		});
-		dialogRef.afterClosed().subscribe(result => {
-			if(result != undefined) {
-				debugger;
-				var item = this.formContainer.find(x=>x.name == result);
-				console.log('The dialog was closed');
-				this.formContainer.splice(i, 1, result);
-				//this.animal = result;
-			}
-		});
-	}
-
-	editMultiSelectProperty(control: any, i: any):void {
-		let dialogRef = this.dialog.open(EditMultiSelectComponent, {
-			disableClose: true,
-			width: '744px',
-			data: control
-		});
-		dialogRef.afterClosed().subscribe(result => {
-			if(result != undefined) {
-				var item = this.formContainer.find(x=>x.name == result);
-				console.log('The dialog was closed');
-				this.formContainer.splice(i, 1, result);
-				//this.animal = result;
-			}
-		});
-	}
-
-	editButtonProperty(control: any, i: any): void {
-		let dialogRef = this.dialog.open(EditButtonComponent, {
-			disableClose: true,
-			width: '744px',
-			data: control
-		});
-		dialogRef.afterClosed().subscribe(result => {
-			if(result != undefined) {
-				var item = this.formContainer.find(x=>x.name == result);
-				console.log('The dialog was closed');
-				this.formContainer.splice(i, 1, result);
-				//this.animal = result;
-			}
-		});
-	}
+	}	
 }
