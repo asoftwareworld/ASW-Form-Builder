e9dc1fb9d7b62a7248fa9eae78ff64ef53bad0f2
anishsharma8 <anishsharma927@gmail.com>
2020-09-25 16:06:52 +0530

    Fixed edit options fields

--- src/app/layout/edit-controls/edit-select/edit-select.component.ts
+++ src/app/layout/edit-controls/edit-select/edit-select.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'asw-edit-select',
@@ -9,6 +10,13 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class EditSelectComponent implements OnInit {
 
     status: boolean;
+    name: string;
+    label: string;
+    tooltip: string;
+    style: string;
+    isRequired: boolean;
+    options: any[] = [];
+    model: any = {};
     option: any = {
         "key": "",
         "value": ""
@@ -18,7 +26,7 @@ export class EditSelectComponent implements OnInit {
         @Inject(MAT_DIALOG_DATA) public control: any) { }
 
     ngOnInit() {
-
+        this.setValue(this.control);
      }
 
     onNoClick(): void {
@@ -26,14 +34,35 @@ export class EditSelectComponent implements OnInit {
     }
 
     addNewOption(): void {
-        this.control.options.push(this.option);
+        this.options.push(this.option);
     }
     removeOption(index: number): void {
-        this.control.options.splice(index, 1);
+        this.options.splice(index, 1);
     }
 
-    onSubmit() {
-        this.dialogRef.close(this.control);
+    onSubmit(aswEditPropertyForm: NgForm) {
+        this.model.displayName = this.control.displayName;
+        this.model.controlType = this.control.controlType;
+        this.model.name = this.name;
+        this.model.label = this.label;
+        this.model.tooltip = this.tooltip;
+        this.model.style = this.style;
+        this.model.isRequired = this.isRequired;
+        this.model.options = this.options;
+        this.dialogRef.close(this.model);
+    }
+
+    setValue(control: any) {
+        this.name = control.name;
+        this.label = control.label;
+        this.tooltip = control.tooltip;
+        this.style = control.style;
+        this.isRequired = control.isRequired;
+        control.options.forEach(element => {
+            let key = element.key;
+            let value = element.value;
+            this.options.push({key, value});
+        });
     }
 
     onChange(event: any) {
