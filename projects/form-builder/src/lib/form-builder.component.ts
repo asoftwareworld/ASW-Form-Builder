81de60dbe5ab883f7b61c6d4bf27bf794de6c74c
anishsharma8 <anishsharma927@gmail.com>
2020-10-07 12:45:51 +0530

    Modified project

--- projects/form-builder/src/lib/form-builder.component.ts
+++ projects/form-builder/src/lib/form-builder.component.ts
@@ -5,76 +5,6 @@ import { Constants } from './common/constants';
 import { JsonPreviewDialogComponent } from './shared-components/json-preview-dialog/json-preview-dialog.component';
 import { ASWSettingsService } from './shared-service/asw-settings.service';
 
-@Component({
-  selector: 'asw-form-builder',
-  templateUrl: './form-builder.component.html',
-  styleUrls: ['./form-builder.component.scss'],
-  providers:[ASWSettingsService]
-})
-export class FormBuilderComponent implements OnInit {
-	
-  	constants: any = Constants;
-	availableControls: any[] = [];
-	formContainer: any[] = [];
-
-	constructor(public dialog: MatDialog,
-		private aswSettingsService: ASWSettingsService) { }
-
-  	ngOnInit(): void {
-		this.formContainer = this.aswSettingsService.previewData;
-		this.availableControls = CONTROLS;
-		// this.aswSettingsService.getJSON().subscribe(data => {
-		// 	this.availableControls = data;
-        // });		
-	}
-
-	drop(event: CdkDragDrop<string[]>) {
-		if (event.previousContainer === event.container) {
-			moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-		} else {
-		copyArrayItem(event.previousContainer.data,
-							event.container.data,
-							event.previousIndex,
-							event.currentIndex);
-		}
-	}
-
-	gridDrop(event: CdkDragDrop<string[]>) {
-		if (event.previousContainer === event.container) {
-			moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-		} else {
-		copyArrayItem(event.previousContainer.data,
-							event.container.data,
-							event.previousIndex,
-							event.currentIndex);
-		}
-	}
-
-	updatedControl(data: any) {
-		this.formContainer.splice(data.index, 1, data.control);
-	}
-
-	deleteControl(index: any) {
-		this.formContainer.splice(index, 1);
-	}
-	
-	// previewTemplate() {
-	// 	this.aswSettingsService.previewData = this.formContainer;
-	// 	this.router.navigate(['preview-template']);
-	// }
-
-	previewJsonData() {
-		let dialogRef = this.dialog.open(JsonPreviewDialogComponent, {
-			disableClose: true,
-			width: '744px',
-			data: this.formContainer
-		});
-		dialogRef.afterClosed();
-	}
-
-}
-
-
 const CONTROLS: any[] = [
 	{ 
 		'icon': 'title', 
@@ -223,16 +153,74 @@ const CONTROLS: any[] = [
 		'label': 'Slide me!',
 		'color':'primary',
 		'isRequired': false
-	},
-	{
-		'icon': 'grid',
-		'displayName': 'Grid',
-		'controlType': 'grid',
-		'columns': [{
-			'components':[]
-		},
-		{
-			'components':[]
-		}]
 	}
 ];
+
+@Component({
+  selector: 'asw-form-builder',
+  templateUrl: './form-builder.component.html',
+  styleUrls: ['./form-builder.component.scss'],
+  providers:[ASWSettingsService]
+})
+export class FormBuilderComponent implements OnInit {
+	
+  	constants: any = Constants;
+	availableControls: any[] = [];
+	formContainer: any[] = [];
+
+	constructor(public dialog: MatDialog,
+		private aswSettingsService: ASWSettingsService) { }
+
+  	ngOnInit(): void {
+		this.formContainer = this.aswSettingsService.previewData;
+		this.availableControls = CONTROLS;
+		// this.aswSettingsService.getJSON().subscribe(data => {
+		// 	this.availableControls = data;
+        // });		
+	}
+
+	drop(event: CdkDragDrop<string[]>) {
+		if (event.previousContainer === event.container) {
+			moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+		} else {
+		copyArrayItem(event.previousContainer.data,
+							event.container.data,
+							event.previousIndex,
+							event.currentIndex);
+		}
+	}
+
+	gridDrop(event: CdkDragDrop<string[]>) {
+		if (event.previousContainer === event.container) {
+			moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+		} else {
+		copyArrayItem(event.previousContainer.data,
+							event.container.data,
+							event.previousIndex,
+							event.currentIndex);
+		}
+	}
+
+	updatedControl(data: any) {
+		this.formContainer.splice(data.index, 1, data.control);
+	}
+
+	deleteControl(index: any) {
+		this.formContainer.splice(index, 1);
+	}
+	
+	// previewTemplate() {
+	// 	this.aswSettingsService.previewData = this.formContainer;
+	// 	this.router.navigate(['preview-template']);
+	// }
+
+	previewJsonData() {
+		let dialogRef = this.dialog.open(JsonPreviewDialogComponent, {
+			disableClose: true,
+			width: '744px',
+			data: this.formContainer
+		});
+		dialogRef.afterClosed();
+	}
+
+}
