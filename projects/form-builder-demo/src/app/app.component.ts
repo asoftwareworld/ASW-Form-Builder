af2a4a26bb80b53311529ddf2dcc95b00d6b9558
anishsharma8 <anishsharma927@gmail.com>
2020-10-10 00:51:55 +0530

    code refcator

--- projects/form-builder-demo/src/app/app.component.ts
+++ projects/form-builder-demo/src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'asw-root',
-  template: `
-  <asw-form-builder (publishClick)="saveJsonData($event)"></asw-form-builder>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
