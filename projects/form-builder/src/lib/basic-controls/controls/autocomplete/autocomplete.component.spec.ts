37811073de1f569535061c09f54d1412067b1c28
asoftwareworld <asoftwareworld@gmail.com>
2020-10-12 12:06:39 +0530

    modified unit test cases

--- projects/form-builder/src/lib/basic-controls/controls/autocomplete/autocomplete.component.spec.ts
+++ projects/form-builder/src/lib/basic-controls/controls/autocomplete/autocomplete.component.spec.ts
@@ -1,11 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { AutocompleteComponent } from './autocomplete.component';
 
 describe('AutocompleteComponent', () => {
   let component: AutocompleteComponent;
   let fixture: ComponentFixture<AutocompleteComponent>;
+  let expectedControl: any;
+  let controlDe: DebugElement;
+  let controlEl: HTMLElement;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AutocompleteComponent ]
     })
@@ -15,6 +19,26 @@ describe('AutocompleteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AutocompleteComponent);
     component = fixture.componentInstance;
+    
+    // mock the control supplied by the parent component
+    expectedControl = {
+      'icon': 'corporate_fare',
+      'displayName': 'Autocomplete',
+      'controlType': 'autocomplete',
+      'name': 'autocomplete',
+      'tooltip': 'Select one',
+      'label': 'Autocomplete',
+      'style':'outline',
+      'isRequired': false,
+      'options': [
+        {'key': 'option-1', 'value': 'Option 1'},
+        {'key': 'option-2', 'value': 'Option 2'},
+        {'key': 'option-3', 'value': 'Option 3'}
+      ]
+    }
+
+    // simulate the parent setting the input property with that control
+    component.control = expectedControl;
     fixture.detectChanges();
   });
 
--- projects/form-builder/src/lib/preview-template/preview-template.component.spec.ts
+++ projects/form-builder/src/lib/preview-template/preview-template.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialog } from '@angular/material/dialog';
 import { PreviewTemplateComponent } from './preview-template.component';
 
 describe('PreviewTemplateComponent', () => {
