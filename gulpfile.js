db4e3383918cd944a8e6b0d7304b8abde51d2777
asoftwareworld <asoftwareworld@gmail.com>
2021-04-11 01:52:20 +0530

    implemented css bundling

--- gulpfile.js
+++ gulpfile.js
@@ -5,62 +5,47 @@ var gulp = require('gulp'),
     uglifycss = require('gulp-uglifycss'),
     rename = require('gulp-rename'),
     del = require('del'),
-    flatten = require('gulp-flatten');
+    sass = require('gulp-dart-sass');
 
-// gulp.task('build-css', function() {
-// 	gulp.src([
-//         'src/assets/asw-theming.scss',
-// 		    'src/components/**/*.scss'
-//     ])
-//         .pipe(concat('asw-theming.css'))
-//         .pipe(gulp.dest('dist/resources'));
-// });
+gulp.task('build-css-prod', function() {
+    return gulp.src('src/assets/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(concat('asw-theming.css'))
+        .pipe(gulp.dest('dist/theming'))
+        .pipe(uglifycss({"uglyComments": true}))
+        .pipe(rename('asw-theming.min.css'))
+        .pipe(gulp.dest('dist/theming'));    
+});
 
-// gulp.task('build-css-prod', function() {
-//     gulp.src([
-//         'src/assets/asw-theming.scss'
-//     ])
-//     .pipe(concat('asw-theming.css'))
-//     .pipe(gulp.dest('dist/resources'))
-//     .pipe(uglifycss({"uglyComments": true}))
-//     .pipe(rename('asw-theming.min.css'))
-//     .pipe(gulp.dest('dist/resources'));
-// });
+gulp.task('copy-component-css', function () {
+    return gulp.src([
+        'src/components/**/*.scss',
+        'src/assets/**/*.scss'
+    ])
+    .pipe(gulp.dest('dist/theming'));    
+});
 
-// gulp.task('copy-component-css', function () {
-//     gulp.src([
-//         'src/components/**/*.scss',
-//         'src/assets/**/*.scss'
-//     ])
-//     .pipe(gulp.dest('dist/resources'));
-// });
-
-// gulp.task('images', function() {
-//     return gulp.src(['src/app/components/**/images/*.png', 'src/app/components/**/images/*.gif'])
-//         .pipe(flatten())
-//         .pipe(gulp.dest('dist/resources/images'));
-// });
-
-// gulp.task('themes', function() {
-//     return gulp.src(['src/assets/components/themes/**/*',
-//         '!src/assets/components/themes/soho-*/**/*',
-//         '!src/assets/components/themes/viva-*/**/*',
-//         '!src/assets/components/themes/mira/**/*',
-//         '!src/assets/components/themes/nano/**/*'])
-//         .pipe(gulp.dest('dist/resources/themes'));
-// });
+gulp.task('images', function() {
+    return gulp.src(['blue.svg'])
+        .pipe(gulp.dest('dist'));
+});
 
 //Cleaning previous gulp tasks from project
 gulp.task('clean', function() {
-	del(['dist/resources']);
+	return del(['dist/theming']);
 });
 
 //Copy readme
 gulp.task('readme', function() {
-    gulp.src(['README.md'])
-    .pipe(gulp.dest('dist'));
+    return gulp.src(['README.md', 'LICENSE'])
+        .pipe(gulp.dest('dist'));
 });
 
 //Building project with run sequence
-gulp.task('build-assets', ['readme']);
+gulp.task('build-assets', 
+gulp.series('clean', 
+'copy-component-css', 
+'build-css-prod', 
+'images', 
+'readme'));
 
